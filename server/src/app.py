cccf2ddea6156622c12212d9bd4a451aba462b41
Giovanni Donati <grdonati99@gmail.com>
2026-01-23 14:40:39 -0300

    feat: Update the app file

--- server/src/app.py
+++ server/src/app.py
@@ -1,25 +1,33 @@
 from http import HTTPStatus
 from fastapi import FastAPI
 
-from .schema.UserSchema import UserPublic, UserRequest
+from schema.UserSchema import UserList, UserPublic, UserRequest, UserUpdate
+from schema.UtilsSchema import Message
 
 app = FastAPI()
 
-@app.get('/', status_code=HTTPStatus.OK)
+
+@app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Server started'}
+    return {"message": "Server started"}
 
 
-@app.post('/user/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+@app.get("/user/", status_code=HTTPStatus.OK, response_model=UserList)
+def get_user():
+    return {"users": [{"username": "donati_dev", "email": "donati@dev.com"}]}
+
+
+@app.post("/user/", status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserRequest):
+    user = UserPublic(username=user.username, email=user.email)
     return user
 
 
-@app.patch('/user/{id}')
-def update_user(id: int, user: UserRequest):
-    return user
+@app.patch("/user/{id}", status_code=HTTPStatus.OK, response_model=Message)
+def update_user(id: int, user: UserUpdate):
+    return {"message": "user updated successfully"}
 
 
-@app.delete('/user/{id}')
+@app.delete("/user/{id}", status_code=HTTPStatus.OK, response_model=Message)
 def delete_user(id: int):
-    return {'message':'user deleted successfully'}
+    return {"message": "user deleted successfully"}
